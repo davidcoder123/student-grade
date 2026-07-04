7fc6669e0ec8add73c168a2196436f12b3dcf633
david <davidakabueze067@gmail.com>
2026-07-04 20:38:24 +0100

    added grade functionality

--- index.js
+++ index.js
@@ -1,24 +1,65 @@
 const name = document.getElementById("name");
 const score = document.getElementById("score");
 const image = document.getElementById("image");
-{
+const displayName = document.getElementById("m-name");
+const displayScore = document.getElementById("m-score");
+const displayGrade = document.getElementById("m-grade");
+const displayImage = document.getElementById("img");
+
+const modalOpen = document.getElementById("modal-open");
+const modal = document.getElementById("modal").classList;
+const modalClose = document.getElementById("m-close");
+const modalIn = document.getElementById("modal-in").classList;
+
+//modal
+
+modalOpen.addEventListener("click", (e) => {
+  e.preventDefault();
   //modal
-  const modalOpen = document.getElementById("modal-open");
-  const modal = document.getElementById("modal").classList;
-  const modalClose = document.getElementById("m-close");
-  const modalIn = document.getElementById("modal-in").classList;
+  modal.add("show");
+  modalIn.add("shows");
 
-  modalOpen.addEventListener("click", (e) => {
-    e.preventDefault();
+  //input
+  displayName.innerHTML = name.value;
+  displayScore.innerHTML = score.value;
+  displayImage.setAttribute("src", image.value);
 
-    modal.add("show");
-    modalIn.add("shows");
-  });
+  //score
+  const scoreNum = Number(score.value);
 
-  modalClose.addEventListener("click", (e) => {
-    e.preventDefault();
+  displayGrade.textContent = grade(scoreNum);
+});
 
-    modal.remove("show");
-    modalIn.remove("shows");
-  });
+modalClose.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  modal.remove("show");
+  modalIn.remove("shows");
+});
+
+//grade
+function grade(score) {
+  if (Number.isNaN(score)) {
+    return "Type a valid number";
+  }
+
+  switch (true) {
+    case score < 0 || score > 100:
+      return "You didn't write the exam";
+
+    case score >= 70:
+      return "GRADE A";
+
+    case score >= 60:
+      return "GRADE B";
+
+    case score >= 50:
+      return "GRADE C";
+
+    case score >= 45:
+      return "GRADE E";
+
+    default:
+      return "GRADE F";
+  }
 }
